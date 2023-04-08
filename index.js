f3f48035130bbe5e47304cdb341bf5b5ee03a82f
abrarfaiaz0 <abrarfaiaz0@gmail.com>
2023-04-09 00:57:08 +0600

    added html file to serve

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hellosads");
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
+});
