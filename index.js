6fb668b963399db3d2443b1489fa81a96e9277e9
abrarfaiaz0 <abrarfaiaz0@gmail.com>
2023-04-09 15:04:48 +0600

    added dist directory

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/dist/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
   socket.on("disconnect", () => {
     console.log("user disconnected");
