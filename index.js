959911fbccf951246e149a6ca0118afd6a8e8cb7
abrarfaiaz0 <abrarfaiaz0@gmail.com>
2023-04-09 15:29:02 +0600

    added public directory

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("listening on port 3000");
+server.listen(5500, () => {
+  console.log("listening on port 5500");
 });
